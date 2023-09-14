742cd858b0085d018ba3ffc41a4ada95da5a376e
aanacarolina <aanacarolina@gmail.com>
2023-09-13 23:14:22 -0300

    ok - norminette

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: anacaro3 <anacaro3@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 17:36:31 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/09/13 23:08:31 by anacaro3         ###   ########.fr       */
+/*   Updated: 2023/09/13 23:13:36 by anacaro3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char *argv[], char *envp[])
 	if (argc == 1)
 	{
 		while (1)
-		printf("readline($< )");// TODO: WHY!!!!?
+			printf("readline($< )");// TODO: WHY!!!!?
 	}
 	return (0);
 }
